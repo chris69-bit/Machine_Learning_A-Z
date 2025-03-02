73f047bbda7597be798e475181c8b3ff4aebcae5
chrispined <chrisiouda9@gmail.com>
2025-03-02 19:38:52 +0300

    Polishing Python Skills

---
+++ CodeBasics/break.py
@@ -0,0 +1,18 @@
+# Tried using a reserved keyword as a variable name
+#break = 5
+#print(break) # SyntaxError: invalid syntax
+
+# Age calculation
+#birth_year = 2001
+#current_year = 2025
+
+#age = current_year - birth_year
+#print(age)
+
+# full name display
+#first_name = "chris"
+#second_name = "odhiambo"
+#last_name = "ouda"
+
+#full_name = first_name + " " + second_name + " " + last_name
+#print(full_name) 
