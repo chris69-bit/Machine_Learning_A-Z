5c269890254f526b83fb95a1300f7063bc2b1c2b
chrispined <chrisiouda9@gmail.com>
2025-03-03 13:59:29 +0300

    Updated python polishing

---
+++ CodeBasics/expenses.py
@@ -0,0 +1,32 @@
+#expense = [2200, 2350, 2600, 2130, 2190]
+#more = expense[1] - expense[0]
+
+#print(f"Your expenses was more in Febuary than January by {more} dollars")
+
+#Total_expense_first_quarter = expense[0] + expense[1] + expense[2]
+#print(Total_expense_first_quarter)
+
+#if 2000 in expense:
+#    print("True")
+#else:
+#    print("False")
+    
+#expense.append(1980)
+#print(expense)
+
+#expense[3] = expense[3] - 200
+#print(expense)
+
+heros=['spider man','thor','hulk','iron man','captain america']
+print(len(heros))
+
+heros.append('black panther')
+print(heros)
+heros.remove('black panther')
+
+
+heros.insert(3, 'black panther')
+print(heros)
+
+heros[1:3] = ['doctor strange']
+print(heros)
