9cc1bb2ff6315a43e97bf6cf509a3e54a6a75971
chrispined <chrisiouda9@gmail.com>
2025-03-03 14:48:23 +0300

    Update

---
+++ CodeBasics/country.py
@@ -0,0 +1,31 @@
+#india = ['mumbai', 'delhi', 'bangalore', 'chennai']
+#pakistan = ['lahore', 'karachi', 'islamabad']
+#bangladesh = ['dhaka', 'khulna', 'rangpur']
+
+#city = input("Enter a city: ").lower().strip()
+
+#if city in india:
+#    print(f"{city} is in India")
+#elif city in pakistan:
+#    print(f"{city} is in Pakistan")
+#elif city in bangladesh:
+#    print(f"{city} is in Bangladesh")
+#else:
+#    print(f"{city} is not in any of the countries")
+
+# india = ['mumbai', 'delhi', 'bangalore', 'chennai']
+# pakistan = ['lahore', 'karachi', 'islamabad']
+# bangladesh = ['dhaka', 'khulna', 'rangpur']
+
+# city1 = input("Enter first city: ").lower().strip()
+# city2 = input("Enter second city: ").lower().strip()
+
+# if city1 in india and city2 in india:
+#     print(f"{city1} and {city2} are in India")
+# elif city1 in pakistan and city2 in pakistan:
+#     print(f"{city1} and {city2} are in Pakistan")
+# elif city1 in pakistan and city2 in bangladesh:
+#     print(f"{city1} and {city2} are in Bangladesh")
+# else:
+#     print(f"{city1} and {city2} are not in the same country")
+
