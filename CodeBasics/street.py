900a9ceb14096a4d4234491e9fb47afcd98ee6c3
chrispined <chrisiouda9@gmail.com>
2025-03-03 13:15:58 +0300

    Added a new file

---
+++ CodeBasics/street.py
@@ -0,0 +1,19 @@
+#street = "Karangathe"
+#city = "Juja"
+#country = "Kenya"
+
+#print("Your are Located at: " + street + ", " + city + " " + country) 
+#print(f"You are located at: {street}, {city} {country}")
+
+#earth = "Earth revolves around the sun"
+#place = earth.split()
+#print(place[1])
+
+#x = int(input("Number of veggies: "))
+#y = int(input("Number of friuts: "))
+
+#print(f"I eat {x} veggies and {y} friuts daily")
+
+#wrong = "maina 200 bananas khaye"
+#wrong = wrong.replace("200", "10").replace("bananas", "samosas")
+#print(wrong)
