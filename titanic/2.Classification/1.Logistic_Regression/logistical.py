c34204f17d069c939fb71cd9687064b90bbac5d8
chrispined <chrisiouda9@gmail.com>
2025-03-05 06:20:00 +0300

    Updated the Logistic Regression

--- titanic/2.Classification/1.Logistic_Regression/logistical.py
+++ titanic/2.Classification/1.Logistic_Regression/logistical.py
@@ -11,3 +11,20 @@ y = df.iloc[:, [1]].values
 # Splitting the data into trainig and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_X.fit(X_train)
+
+# Fit the model
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+# Classifying the test set
+y_pred = classifier.predict(X_test)
+
+#Confusion matrix 
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
