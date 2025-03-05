61d63b57e07ebf573154afffab649a8c6899e0af
chrispined <chrisiouda9@gmail.com>
2025-03-05 18:55:21 +0300

    Update

---
+++ CodeBasics/heads.py
@@ -0,0 +1,8 @@
+result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
+
+count = 0
+for i in result:
+    if i == "heads":
+        count += 1
+        
+print(f"Head Count: {count}")
