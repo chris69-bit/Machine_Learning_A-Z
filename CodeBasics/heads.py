7c2838d4cc4e41bd9ab7adfe71fddb7b3f631402
chrispined <chrisiouda9@gmail.com>
2025-03-06 07:22:51 +0300

    Updates

--- CodeBasics/heads.py
+++ CodeBasics/heads.py
@@ -1,8 +1,20 @@
-result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
+#result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
 
-count = 0
-for i in result:
-    if i == "heads":
-        count += 1
+#count = 0
+#for i in result:
+#    if i == "heads":
+#        count += 1
         
-print(f"Head Count: {count}")
+#print(f"Head Count: {count}")
+
+expense_list = [2340, 2500, 2100, 3100, 2980]
+
+amount = int(input("Enter the amount: "))
+
+for i in expense_list:
+    if i == amount:
+        print("Amount found")
+        break
+    else:
+        print("Amount not found")
+        break
