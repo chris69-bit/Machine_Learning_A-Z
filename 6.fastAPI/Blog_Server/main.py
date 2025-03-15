68c87e85560a185aaf7082cf868d4f551e9392b7
chrispined <chrisiouda9@gmail.com>
2025-03-16 02:02:58 +0300

    Added User

--- 6.fastAPI/Blog_Server/main.py
+++ 6.fastAPI/Blog_Server/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 import models
 from database import engine, SessionLocal
-from schemas import Blog
+from schemas import Blog, BlogExtended, User, ShowUser
 from sqlalchemy.orm import Session
-# import uvicorn
+from typing import List
+from hashing import Hash
+#from passlib.context import CryptContext
+#import uvicorn
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -31,17 +34,17 @@ def create_blog(request: Blog, db: Session = Depends(get_db)):
 
 # Read Operations
 
-@app.get("/blog")
+@app.get("/blog") #response_model=List[BlogExtended]
 def all(db: Session = Depends(get_db)):
     blog = db.query(models.Blog).all()
     return blog
 
-@app.get("/blog/unpublished")
-def unpublished():
-    return {"data": "Blog list"}
+# @app.get("/blog/unpublished")
+# def unpublished():
+#     return {"data": "Blog list"}
 
-@app.get("/blog/{id}")
-def page(id: int, response: Response, db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=BlogExtended)
+def page(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -49,9 +52,19 @@ def page(id: int, response: Response, db: Session = Depends(get_db), status_code
     else:
         return blog
 
-@app.get("/blog/{id}/comments")
-def comments(id: int, limit=10):
-    return {"data": "Blog list"}
+# @app.get("/blog/{id}/comments")
+# def comments(id: int, limit=10):
+#     return {"data": "Blog list"}
+
+#Update Items
+@app.put("/blog/{id}", status_code = status.HTTP_202_ACCEPTED)
+def update(id: int, request: Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(request)
+    db.commit()
+    return{"details": "Blog updated successfully"}
 
 # Delete Items
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -63,16 +76,19 @@ def delete_blog(id: int, db: Session = Depends(get_db)):
     db.commit()
     return {"details" : "Blog deleted successfully"}
 
-#Update Items
-@app.put("/blog/{id}", status_code = status.HTTP_202_ACCEPTED)
-def update(id: int, request: Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    blog.update(request)
+
+   
+#Adding a user to the database
+
+
+@app.post("/user", status_code = status.HTTP_201_CREATED, response_model=ShowUser)
+def create_user(request: User, db: Session = Depends(get_db)):
+    new_user = models.User(username=request.username, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
     db.commit()
-    return{"details": "Blog updated successfully"}
+    db.refresh(new_user)
     
+    return new_user
 
     
 # if __name__ == "__main__":
--- 6.fastAPI/Blog_Server/schemas.py
+++ 6.fastAPI/Blog_Server/schemas.py
@@ -6,3 +6,22 @@ class Blog(BaseModel):
     title: str
     body: str
     published: Optional[bool]
+    
+# Extend the Blog model
+class BlogExtended(Blog):
+    title: str
+    body: str
+    class Config:
+        orm_mode = True
+        
+# Create a User
+class User(BaseModel):
+    username: str
+    email:str
+    password: str
+
+class ShowUser(BaseModel):
+    username: str
+    email: str
+    class Config:
+        orm_mode = True
